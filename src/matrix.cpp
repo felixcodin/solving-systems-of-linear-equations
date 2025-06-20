8c6e8522aed3147f6cc55ea53f316e42e14f4ed8
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2025-06-20 14:48:40 +0700

    Update matrix.cpp file

---
+++ src/matrix.cpp
@@ -0,0 +1,30 @@
+#include "matrix.hpp"
+#include <fstream>
+#include <iostream>
+
+Matrix::Matrix(size_t n) : n(n), matrix(n, std::vector<double>(n, 0.0)) {}
+
+double &Matrix::operator()(size_t i, size_t j) { return matrix[i][j]; }
+const double &Matrix::operator()(size_t i, size_t j) const { return matrix[i][j]; }
+
+Matrix Matrix::loadFromFile(const std::string &filename)
+{
+    std::ifstream in(filename);
+    size_t sz;
+    in >> sz;
+    Matrix M(sz);
+    for (int i = 0; i < sz; i++)
+        for (int j = 0; j < sz; j++)
+            in >> M(i, j);
+    return M;
+}
+
+void Matrix::print() const
+{
+    for (const auto &row : matrix)
+    {
+        for (double value : row)
+            std::cout << value << ' ';
+        std::cout << '\n'; 
+    }
+}
